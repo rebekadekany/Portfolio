6e5555fcde84fc65a1079762f97f5cd11d285102
Orsolya <o.dekany@gmail.com>
2020-03-23 12:04:06 +0100

    adding MobileMenu header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,7 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
-import NavUp from './modules/NavUp'
-
 
 let mobilemenu = new MobileMenu();
-let navup = new NavUp();
 
 
 if (module.hot) {
--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,7 +1,7 @@
 class MobileMenu {
     constructor() {
         this.menuIcon = document.querySelector(".header__menu-icon")
-        this.menuContent = document.querySelector (".header__navbar")
+        this.menuContent = document.querySelector (".header__menu-content")
         this.header = document.querySelector(".header")
         this.events()
     }
@@ -11,7 +11,7 @@ class MobileMenu {
     }
 
     toggleTheMenu() {
-        this.menuContent.classList.toggle("header__navbar--is-visible")
+        this.menuContent.classList.toggle("header__menu-content--is-visible")
         this.header.classList.toggle("header--is-expanded")
         this.menuIcon.classList.toggle("header__menu-icon--close-x")
     }
